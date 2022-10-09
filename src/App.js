1251c41f9401c5034657d2be0f0266e6266dbab5
yaoqing1119 <304707640@qq.com>
2022-10-09 18:33:45 +0800

    3 files modified

--- src/App.js
+++ src/App.js
@@ -1,4 +1,15 @@
 import React, { useState } from 'react';
+import { connect } from 'react-redux';
+
+const MyChild = (props) => {
+  return <div>child--{props.sName}</div>;
+};
+
+const Child = connect((state) => {
+  return {
+    sName: state.name,
+  };
+})(MyChild);
 
 export default function App() {
   let [user, setUser] = useState({ name: 'yq', age: 18 });
@@ -6,6 +17,7 @@ export default function App() {
     <div onClick={() => setUser({ name: 'carrey', age: 7.5 })}>
       <h1>{user.name}</h1>
       <p>{user.age}</p>
+      <Child a="1"></Child>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-
+import { legacy_createStore } from 'redux';
+import { Provider } from 'react-redux';
 import App from './App';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
+const store = legacy_createStore(
+  function (state, payload) {
+    switch (payload.type) {
+      case 'CHANGE_NAME':
+        return { ...state, name: payload.name };
+      default:
+        return state;
+    }
+  },
+  { name: 'store_yq' }
+);
+
 root.render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>
 );
