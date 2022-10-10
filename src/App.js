789f527c5ac6b275d084be7b08e324eb75ca110e
yaoqing1119 <304707640@qq.com>
2022-10-10 11:23:38 +0800

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
-import React, { useState } from 'react';
+import React, {
+  useState,
+  useImperativeHandle,
+  forwardRef,
+  useRef,
+} from 'react';
 import { connect } from 'react-redux';
-import './style.css'
-const MyChild = (props) => {
-  return <div>child--{props.sName}</div>;
-};
-
-const Child = connect((state) => {
-  return {
-    sName: state.name,
+import './style.css';
+const MyChild = (props, ref) => {
+  useImperativeHandle(ref, () => ({
+    myHandler,
+  }));
+  const myHandler = () => {
+    console.log('myhandler');
   };
-})(MyChild);
+
+  return (
+    <div onClick={props.changeName}>
+      <p>child--{props.sName}</p>
+      <button onClick={myHandler}>xxx</button>
+    </div>
+  );
+};
+const Forward = forwardRef(MyChild);
+const Child = connect(
+  (state) => {
+    return {
+      sName: state.name,
+    };
+  },
+  (dispatch) => ({
+    changeName(e) {
+      e.stopPropagation();
+      dispatch({ type: 'CHANGE_NAME', name: 'change_store_all' });
+    },
+  })
+)(Forward);
 
 export default function App() {
   let [user, setUser] = useState({ name: 'yq', age: 18 });
@@ -17,13 +42,18 @@ export default function App() {
   const handler = (e) => {
     e.stopPropagation();
     console.log(123);
-  }
+    ref.current.myHandler();
+  };
+
+  let ref = useRef(null);
 
   return (
     <div onClick={() => setUser({ name: 'carrey', age: 7.5 })}>
       <h1>{user.name}</h1>
-      <p className='box' onClick={handler}>{user.age}</p>
-      <Child a="1"></Child>
+      <p className="box" onClick={handler}>
+        {user.age}
+      </p>
+      <Child a="1" ref={ref}></Child>
     </div>
   );
 }
